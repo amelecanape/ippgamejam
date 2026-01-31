292147f4a020329004c149f0488a25e484f69634
loco-choco <contact@locochoco.dev>
2026-01-31 21:36:04 +0100

    fixed fox powert

--- scripts/masks/fox_mask_power.gd
+++ scripts/masks/fox_mask_power.gd
@@ -4,12 +4,16 @@ class_name FoxMaskPower extends MaskPower
 @export var invisible_counter : float = 0
 
 func _process(delta: float) -> void:
-	mask.masked_character.character_sprite.visible = true
+	_become_visible()
 	if invisible_counter > 0:
-		mask.masked_character.character_sprite.visible = false
+		_become_visible(false)
 		
 	invisible_counter = invisible_counter - delta \
 					 if invisible_counter > 0 else  0.0
+
+func _become_visible(visible : bool = true) -> void:
+	mask.masked_character.character_sprite.visible = visible
+	mask.sprite.visible = visible
 
 func execute_ability() -> void:
 	invisible_counter = invisibility_duration
