a35a0bf43b5111d3fc40e5777fe0683befa9c980
loco-choco <contact@locochoco.dev>
2026-01-31 01:21:43 +0100

    added tilemaps and other stuff

--- scripts/npc_control.gd
+++ scripts/npc_control.gd
@@ -1,15 +1,19 @@
 class_name NPCControl extends MaskedCharacter
 
-var target : Node2D = null
-
 @onready var navigation_agent : NavigationAgent2D = $%NavigationAgent
 
 @export var DEBUG : bool = false
 
 func _process(_delta: float) -> void:
 	if DEBUG and Input.is_action_just_pressed("debug_mouse_left_click"):
-		navigation_agent.target_position = get_global_mouse_position()
+		set_movement_target(get_global_mouse_position())
+
+func set_movement_target(movement_target: Vector2):
+	navigation_agent.target_position = movement_target
+		
 func _physics_process(_delta: float) -> void:
+	if navigation_agent.is_navigation_finished():
+		return
 	var next_pos : Vector2 = navigation_agent.get_next_path_position()
-	mov_input = (next_pos - global_position).normalized()
+	mov_input = global_position.direction_to(next_pos).normalized()
 	super._physics_process(_delta)
